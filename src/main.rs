9278669bc756c4ea9481cffda4660cb9fb1dea37
dorkpad <creek.frogs@gmail.com>
2026-05-20 02:27:01 -0700

    add clock tracking, update frontend

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
 mod templates;
 use templates::IndexTemplate;
 use askama::Template;
+use chrono::Local;
+use chrono::Datelike;
+use chrono::Timelike;
 
 fn main() {
-    let tmpl = IndexTemplate { user: "Michael".to_string() };
+    let now = Local::now();
+    let day_of_year = now.ordinal();
+
+    let tmpl = IndexTemplate { 
+        month: now.month(),
+        day:   now.day(),
+        year:  now.year(),
+        hour:  now.hour(),
+        minute:now.minute(),
+        second:now.second(),
+        day_count: day_of_year,
+    };
     println!("{}", tmpl.render().unwrap());
 }
--- src/templates.rs
+++ src/templates.rs
@@ -3,5 +3,11 @@ use askama::Template;
 #[derive(Template)]
 #[template(path = "index.html")]
 pub struct IndexTemplate {
-    pub user: String,
+    pub month: u32,
+    pub day: u32,
+    pub year: i32,
+    pub hour: u32,
+    pub minute: u32,
+    pub second: u32,
+    pub day_count: u32,
 }
