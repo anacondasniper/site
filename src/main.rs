01509c7a89a2bf4568ecf9cdb01c1a849dbda10b
dorkpad <creek.frogs@gmail.com>
2026-05-21 15:43:59 -0700

    CSS working on localhost

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 mod handlers;
 mod templates;
+use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
-    let app = axum::Router::new().route("/", axum::routing::get(handlers::index));
+    let app = axum::Router::new()
+        .route("/", axum::routing::get(handlers::index))
+        .nest_service("/static", ServeDir::new("static"));
+
     // .route("/thread{id}", axum::routing::get(handlers::thread));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
